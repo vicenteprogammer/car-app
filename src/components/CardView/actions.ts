d8c7d6ac451d0be81bd2dd6787123210e72cc094
vicente <vicentejunior4522@gmail.com>
2025-08-21 09:45:28 -0300

    Finally app

--- src/components/CardView/actions.ts
+++ src/components/CardView/actions.ts
@@ -1,13 +1,34 @@
 //Onde fica as ações que nosso componente irá fazer, principas funções.
 
-export const loadCarData = () =>{
+import { fetchCars } from "../../api/getCars"
+import { carModel } from "./props"
+
+export const loadCarData = async  (id:number, setCarData: React.Dispatch<React.SetStateAction<carModel | null>>) =>{
+  try{
+    const response = await fetchCars(id);
+    setCarData(response)
+  }catch(e){
+    console.error(e);
+    setCarData(null)
+  }
 
 }
 
-export const nextCar = () =>{
+export const nextCar = async (carData: carModel | null,setCarData: React.Dispatch<React.SetStateAction<carModel | null>> ) =>{
+  let response = null
+
+  if(carData && carData.id < 10){
+    response = await fetchCars(carData.id + 1)
+    setCarData(response)
+  }
 
 }
 
-export const prevCar = () =>{
+export const prevCar = async (carData: carModel | null,setCarData: React.Dispatch<React.SetStateAction<carModel | null>> ) =>{
+  let response = null
 
+  if(carData && carData.id > 1){
+    response = await fetchCars(carData.id -1)
+    setCarData(response)
+  }
 }
--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Image, Text, View } from 'react-native'
 import { styles } from "./styles";
 import logo from '../../../assets/logo3.png'
 import Divider from "../Divider";
 import { CAR_IMG_URL } from "../../constants/car";
 import BuyButton from "../BuyButton";
+import { carModel } from "./props";
+import { loadCarData, nextCar, prevCar } from "./actions";
 export default function CardView(){
+  const [carData, setCarData] = useState<carModel | null>(null);
+
+  useEffect(() => {
+     (async ()=>{
+      await loadCarData(1, setCarData)
+     })()
+  }, [])
+
+  //Função IIFE -> estrutura (code)()
+
 
   const renderImageBox = () =>{
     return(
@@ -18,24 +30,24 @@ export default function CardView(){
   const renderCarDetails = () =>{
     return(
     <View style={styles.carDetail}>
-      <Text style={styles.carBrand} >Lamborghini</Text>
-      <Text style ={styles.carModel} >Model</Text>
+      <Text style={styles.carBrand} >{carData?.carName}</Text>
+      <Text style ={styles.carModel} >{carData?.releaseYear}</Text>
     </View>
     )
   }
 
   const renderImageCar = () =>{
     return(
-      <Image style={styles.imageCar} source={{uri:`${CAR_IMG_URL}4.png` }}/>
+      <Image style={styles.imageCar} source={{uri:`${CAR_IMG_URL}${carData?.id}.png` }}/>
     )
   }
 
   const priceControls = () =>{
     return(
       <View style={styles.labelControlStyle}>
-        <Button title="<" color={'#7595e4ff'} onPress={()=>{}} />
-        <Text style={styles.textLabel} >Value</Text>
-        <Button title=">"  color={'#7595e4ff'} onPress={()=>{}} />
+        <Button title="<" color={'#7595e4ff'} onPress={()=> prevCar(carData, setCarData)} />
+        <Text style={styles.textLabel} >{carData?.price}</Text>
+        <Button title=">"  color={'#7595e4ff'} onPress={()=> nextCar(carData, setCarData)} />
       </View>
     )
   }
